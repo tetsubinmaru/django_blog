983fb9686dabff8901f81feb7cd68e343ef86d61
tetsubinmaru <tesoro5963@gmail.com>
2021-03-17 20:54:11 +0900

    third commit

--- blogs/urls.py
+++ blogs/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'blogs'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('detail/<int:blog_id>', views.detail, name='detail'),
+    path('new/', views.new, name='new'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirects, get_object_or_404
 from .models import Blog
+from .forms import BlogForm
 
 def index(request):
     blogs = Blog.objects.order_by("-created_datetime")
     return render(request, 'blogs/index.html', {"blogs": blogs})
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, id=blog_id)
+    return render(request, 'blogs/detail.html', {"blog": blog})
+
+def new(request):
+    if request.method == "POST":
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:index')
+        else:
+            form = BlogForm
+        return render(request, 'blogs/new.html', {'form': form})
